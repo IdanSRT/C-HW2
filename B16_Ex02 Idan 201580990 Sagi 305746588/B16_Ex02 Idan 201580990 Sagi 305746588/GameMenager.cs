4ce1cc3f30f1e0a27d7485ee0d9da8c5f1b15d95
Idan.Shuraty <idanshuraty89@gmail.com>
2016-05-14 17:09:49 +0300

    classes Player ang GameManager

---
+++ B16_Ex02 Idan 201580990 Sagi 305746588/B16_Ex02 Idan 201580990 Sagi 305746588/GameMenager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B16_Ex02_Idan_201580990_Sagi_305746588
+{
+    class GameMenager
+    {
+        Board m_GameBoard;
+        Player m_FirstPlayer;
+        Player m_SecondPlayer;
+
+     
+
+        public GameMenager(Board i_GameBoard, String i_FirstPlayerName, String i_SecondPlayerName)
+        {
+            m_GameBoard = i_GameBoard;
+            m_FirstPlayer.Name = i_FirstPlayerName;
+            m_SecondPlayer.Name = i_SecondPlayerName;
+        }
+
+        public GameMenager(Board i_GameBoard, String i_FirstPlayerName)
+        {
+           new GameMenager(i_GameBoard, i_FirstPlayerName, "Computer");
+        }
+
+
+    }
+}
