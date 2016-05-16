49493e21f469354a24cf9d10f8954af130dc031b
Idan.Shuraty <idanshuraty89@gmail.com>
2016-05-16 18:27:21 +0300

    debugg

--- B16_Ex02 Idan 201580990 Sagi 305746588/B16_Ex02 Idan 201580990 Sagi 305746588/GameMenager.cs
+++ B16_Ex02 Idan 201580990 Sagi 305746588/B16_Ex02 Idan 201580990 Sagi 305746588/GameMenager.cs
@@ -105,10 +105,10 @@ namespace B16_Ex02_Idan_201580990_Sagi_305746588
                     goodInput = true;
                     Console.WriteLine(columnChooseInt);
                 }
-
-                while (!goodInput || columnChooseInt > m_ColumnRange || columnChooseInt < 1)
+                
+                while (!goodInput || columnChooseInt > m_ColumnRange || columnChooseInt < 1 || m_GameBoard.IsColumnFull(columnChooseInt - 1))
                 {
-                    if (m_GameBoard.IsColumnFull(columnChooseInt))
+                    if (m_GameBoard.IsColumnFull(columnChooseInt - 1))
                     {
                         Console.WriteLine("Column " + columnChooseInt + "is full.\nPlease choose a different column:");
                     }
